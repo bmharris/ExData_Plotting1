e4bab6f37d9b10cd45bc6764c989f50539991e93
bharris <bharris05@gmail.com>
2016-01-09 20:35:06 -0500

    Minor Change

--- plot4.R
+++ plot4.R
@@ -21,10 +21,15 @@ project_data$Sub_metering_3 <- as.numeric(as.character(project_data$Sub_metering
 project_data$Voltage <- as.numeric(as.character(project_data$Voltage))
 project_data$Global_reactive_power <- as.numeric(as.character(project_data$Global_reactive_power))
 
-#build Plot 3
-png("plot3.png", w = 480, h = 480)
+
+# build plot 4
+png("plot4.png", w = 480, h = 480)
+par(mfrow = c(2,2))
+with(project_data, plot(date_time, Global_active_power, type = "l", ylab = "Global Active Power", xlab = ""))
+with(project_data, plot(date_time, Voltage, type = "l", ylab = "Voltage", xlab = "datetime"))
 with(project_data, plot(date_time,Sub_metering_1, type = "l", col = "black", ylab = "Energy sub metering", xlab = ""))
 with(project_data, lines(date_time,Sub_metering_2, type = "l", col = "red" ))
 with(project_data, lines(date_time,Sub_metering_3, type = "l", col = "blue" ))
-legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = c(1,1,1), col = c("black", "red", "blue"))
+legend("topright", border = "white", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = c(1,1,1), col = c("black", "red", "blue"), bty = "n")
+with(project_data, plot(date_time, Global_reactive_power, type = "l", ylab = "Global_reactive_power", xlab = ""))
 dev.off()
